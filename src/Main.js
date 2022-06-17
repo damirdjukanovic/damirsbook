7a47e249a90aef051911d0a602ef406451fcaf03
damirdjukanovic <damir.djukanovic123@gmail.com>
2022-06-17 15:24:23 +0200

    changes made

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Home from "./pages/homepage/Home";
 import Profile from "./pages/profile/Profile";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Messenger from "./pages/messenger/Messenger";
 import store from "./store";
-import {getMessages} from "./actions/messageActions";
+import { getMessages } from "./actions/messageActions";
 import socket from "./Socket";
 import { LoopCircleLoading } from 'react-loadingg';
-import ReduxToastr from 'react-redux-toastr'; 
+import ReduxToastr from 'react-redux-toastr';
 
 import {
   Switch,
@@ -26,14 +26,14 @@ export const NotAuthorized = () => {
 
 const Main = (props) => {
 
-  const {isAuthenticated, isLoading, user} = props;
+  const { isAuthenticated, isLoading, user } = props;
   const [onlineUsers, setOnlineUsers] = useState([]);
-
+  console.log('test')
   useEffect(() => {
-    if(user) {
+    if (user) {
       store.dispatch(getMessages(user._id));
     }
-    if(isAuthenticated) {
+    if (isAuthenticated) {
       socket.emit("addUser", user._id);
       socket.on("getUsers", (users) => {
         setOnlineUsers(
@@ -43,15 +43,15 @@ const Main = (props) => {
     }
   }, [user, isAuthenticated]);
 
-  if(isLoading) {
+  if (isLoading) {
     return <LoopCircleLoading />
   }
-  
+
   return (
-        <Switch>
-        <Route exact path="/home">
-          {isAuthenticated ? <Home onlineUsers={onlineUsers} /> : <Login />}
-          <ReduxToastr
+    <Switch>
+      <Route exact path="/home">
+        {isAuthenticated ? <Home onlineUsers={onlineUsers} /> : <Login />}
+        <ReduxToastr
           timeOut={4000}
           newestOnTop={false}
           preventDuplicates
@@ -59,17 +59,17 @@ const Main = (props) => {
           getState={(state) => state.toastr}
           transitionIn="fadeIn"
           transitionOut="fadeOut"
-          closeOnToastrClick/> 
-         </Route>
-         <Route exact path="/">
-          {isAuthenticated ? <Redirect to="/home" /> : <Login />}
-         </Route>
-         <Route exact path="/register">
-           {isAuthenticated ? <Redirect to="/home" /> : <Register />}
-         </Route>
-         <Route exact path="/profile/:username">
-         {isAuthenticated ? <Profile user={user} onlineUsers={onlineUsers}/> : <LoopCircleLoading />}
-         <ReduxToastr
+          closeOnToastrClick />
+      </Route>
+      <Route exact path="/">
+        {isAuthenticated ? <Redirect to="/home" /> : <Login />}
+      </Route>
+      <Route exact path="/register">
+        {isAuthenticated ? <Redirect to="/home" /> : <Register />}
+      </Route>
+      <Route exact path="/profile/:username">
+        {isAuthenticated ? <Profile user={user} onlineUsers={onlineUsers} /> : <LoopCircleLoading />}
+        <ReduxToastr
           timeOut={4000}
           newestOnTop={false}
           preventDuplicates
@@ -77,15 +77,15 @@ const Main = (props) => {
           getState={(state) => state.toastr}
           transitionIn="fadeIn"
           transitionOut="fadeOut"
-          closeOnToastrClick/>  
-         </Route>
-         <Route exact path="/messenger">
-         {isAuthenticated ? <Messenger onlineUsers={onlineUsers}/> : <LoopCircleLoading />} 
-         </Route>
-         <Route exact path="/messenger/:id">
-         {isAuthenticated ? <Messenger onlineUsers={onlineUsers} /> : <LoopCircleLoading />} 
-         </Route>
-        </Switch>
+          closeOnToastrClick />
+      </Route>
+      <Route exact path="/messenger">
+        {isAuthenticated ? <Messenger onlineUsers={onlineUsers} /> : <LoopCircleLoading />}
+      </Route>
+      <Route exact path="/messenger/:id">
+        {isAuthenticated ? <Messenger onlineUsers={onlineUsers} /> : <LoopCircleLoading />}
+      </Route>
+    </Switch>
   );
 }
 
